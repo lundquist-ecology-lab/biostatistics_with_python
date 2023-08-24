efdd9fc74a681bba425247b71374b5016f9dcd96
Matthew Lundquist <lundquistecologylab@gmail.com>
2023-08-24 17:43:28 -0400

    textbook link

---
+++ utilities/textbook_merge.sh
@@ -0,0 +1,19 @@
+
+cd /textbook/ && nbmerge 01_Introduction.ipynb \
+02_Fundamentals_of_data.ipynb \
+03_Displaying_data.ipynb \
+04_Describing_data.ipynb \
+05_Uncertainty.ipynb \
+06_Probability.ipynb \
+07_Hypotheses.ipynb \
+08_Binomial_test.ipynb \
+09_Chi-squared.ipynb \
+10_Parametric_tests.ipynb \
+11_ANOVA.ipynb \
+12_Non-parametric_tests.ipynb \
+13_Predictive_models.ipynb \
+A1_Python_basics.ipynb \
+A2_Jupyer_Notebooks.ipynb \
+A3_Importing_Data.ipynb \
+A4_Python_Libraries.ipynb \
+A5_Notebook_tricks.ipynb > merged.ipynb
